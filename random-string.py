6feaaf66b15fd16fcb604b86b2cc2c8eb69edbb4
Kai <phamtungduong@gmail.com>
2014-02-04 00:41:38 +0700

    add version 2

--- random-string.py
+++ random-string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from random import randint
+from random import randint, choice
 DICT_NAME = "my-short-nouns.dict"
 DICT_LEN = 30089 - 1
 
@@ -14,6 +14,15 @@ def gen_random():
     word1 = get_random_word (randint (0, DICT_LEN)).capitalize()
     word2 = get_random_word (randint (0, DICT_LEN))
     return "{0}-{1}-{2}".format (word1, word2, randint (10, 99))
+
+def get_random2():
+    with open (DICT_NAME) as fin:
+        words = [line[:-1] for pi, line in enumerate (fin)]
+    word1 = choice (words).capitalize()
+    word2 = choice (words)
+    return word1 + "-" + word2 + "-" + str (randint (10, 99))
     
+
 if __name__ == "__main__":
-    print (gen_random())
+    #print (gen_random())
+    print (get_random2())
